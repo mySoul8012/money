1e3189a14a74dc412c700af0f4e28fa7f83c6413
ming <mingming@mingming.email>
2019-09-06 22:29:35 +0800

    :construction:	construction

--- trend-trading-backtest-service/src/main/java/com/ming/service/BackTestService.java
+++ trend-trading-backtest-service/src/main/java/com/ming/service/BackTestService.java
@@ -1,13 +1,15 @@
 package com.ming.service;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
 import com.ming.client.IndexDataClient;
 import com.ming.pojo.IndexData;
+import com.ming.pojo.Profit;
+import com.ming.pojo.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BackTestService {
@@ -15,13 +17,205 @@ public class BackTestService {
     IndexDataClient indexDataClient;
 
     public List<IndexData> listIndexData(String code){
-        List<IndexData> res = indexDataClient.getIndexData(code);
-        Collections.reverse(res);
+        List<IndexData> result = indexDataClient.getIndexData(code);
+        // 降序 排序
+        Collections.reverse(result);
+        return result;
+    }
 
-        for(IndexData indexData: res){
-            System.out.println(indexData.getDate());
+
+    /**
+     * @param ma 均线
+     * @param sellRate 设置的卖出点
+     * @param buyRate 设置的买入点
+     * @param serviceCharge
+     * @param indexDatas
+     * @return
+     */
+    public Map<String,Object> simulate(int ma, float sellRate, float buyRate, float serviceCharge, List<IndexData> indexDatas)  {
+
+
+        // 交易数组
+        List<Trade> trades = new ArrayList<>();
+
+
+        // 利润数组
+        List<Profit> profits =new ArrayList<>();
+        // 初始现金
+        float initCash = 1000;
+        // 现金
+        float cash = initCash;
+        // 份额
+        float share = 0;
+        // 价值
+        float value = 0;
+
+
+        int winCount = 0;
+        float totalWinRate = 0;
+        float avgWinRate = 0;
+        float totalLossRate = 0;
+        int lossCount = 0;
+        float avgLossRate = 0;
+
+
+        // 初始值
+        float init =0;
+
+        // 获取初始值，让其初始值相同
+        if(!indexDatas.isEmpty())
+            init = indexDatas.get(0).getClosePoint();
+
+        // 遍历数组
+        for (int i = 0; i<indexDatas.size() ; i++) {
+            // 取出第一个数据
+            IndexData indexData = indexDatas.get(i);    // 550
+            // 找到收盘价
+            float closePoint = indexData.getClosePoint();   // 604
+            // 20 日均线
+            float avg = getMA(i,ma,indexDatas); // 581
+            // 20 日最大值
+            float max = getMax(i,ma,indexDatas);    // 606
+
+            // 当日增长幅度
+            float increase_rate = closePoint/avg;   // 604 / 581 = 1.2
+            // 当日亏的幅度
+            float decrease_rate = closePoint/max;   // 500 / 450 = 1.11
+
+            // 如果当前没有数据
+            if(avg!=0) {
+                //如果购买超过的均线  buyRate =
+                if(increase_rate > buyRate  ) {
+                    //如果此时还没有买 如果份额等于0
+                    if(0 == share) {
+                        // 当前的现金，除 当前的收盘价 得到当前购买的份额
+                        share = cash / closePoint;
+                        // 买完以后，现金为0
+                        cash = 0;
+
+                        Trade trade = new Trade();
+                        trade.setBuyDate(indexData.getDate());
+                        trade.setBuyClosePoint(indexData.getClosePoint());
+                        trade.setSellDate("n/a");
+                        trade.setSellClosePoint(0);
+                        trades.add(trade);
+                    }
+                }
+                // 如果当日亏的幅度，小于卖点，卖出
+                else if(decrease_rate<sellRate ) {
+                    //如果没卖
+                    if(0!= share){
+                        cash = closePoint * share * (1-serviceCharge);
+                        share = 0;
+
+                        Trade trade =trades.get(trades.size()-1);
+                        trade.setSellDate(indexData.getDate());
+                        trade.setSellClosePoint(indexData.getClosePoint());
+
+                        float rate = cash / initCash;
+                        trade.setRate(rate);
+
+                        if(trade.getSellClosePoint()-trade.getBuyClosePoint()>0) {
+                            totalWinRate +=(trade.getSellClosePoint()-trade.getBuyClosePoint())/trade.getBuyClosePoint();
+                            winCount++;
+                        }
+
+                        else {
+                            totalLossRate +=(trade.getSellClosePoint()-trade.getBuyClosePoint())/trade.getBuyClosePoint();
+                            lossCount ++;
+                        }
+                    }
+                }
+                //do nothing
+                else{
+
+                }
+            }
+
+            // 那么份额为 0
+            if(share!=0) {
+                value = closePoint * share;
+            }
+            // 在份额为 0 的时候，现金和价值相等
+            else {
+                value = cash;
+            }
+            // 收益率
+            float rate = value/initCash;
+
+            Profit profit = new Profit();
+            profit.setDate(indexData.getDate());
+            profit.setValue(rate*init);
+
+            System.out.println("profit.value:" + profit.getValue());
+            // 放入集合里
+            profits.add(profit);
         }
 
-        return res;
+        Map<String,Object> map = new HashMap<>();
+        map.put("profits", profits);
+        map.put("trades", trades);
+
+        map.put("winCount", winCount);
+        map.put("lossCount", lossCount);
+        map.put("avgWinRate", avgWinRate);
+        map.put("avgLossRate", avgLossRate);
+
+        return map;
+    }
+
+    private static float getMax(int i, int day, List<IndexData> list) {
+        int start = i-1-day;
+        if(start<0)
+            start = 0;
+        int now = i-1;
+
+        if(start<0)
+            return 0;
+
+        float max = 0;
+        for (int j = start; j < now; j++) {
+            IndexData bean =list.get(j);
+            if(bean.getClosePoint()>max) {
+                max = bean.getClosePoint();
+            }
+        }
+        return max;
+    }
+
+    private static float getMA(int i, int ma, List<IndexData> list) {
+        int start = i-1-ma;
+        int now = i-1;
+
+        if(start<0)
+            return 0;
+
+        float sum = 0;
+        float avg = 0;
+        for (int j = start; j < now; j++) {
+            IndexData bean =list.get(j);
+            sum += bean.getClosePoint();
+        }
+        avg = sum / (now - start);
+        return avg;
+    }
+
+
+    /**
+     * 获取当前多少年
+     * @param allIndexDatas
+     * @return
+     */
+    public float getYear(List<IndexData> allIndexDatas) {
+        float years;
+        String sDateStart = allIndexDatas.get(0).getDate();
+        String sDateEnd = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+
+        Date dateStart = DateUtil.parse(sDateStart);
+        Date dateEnd = DateUtil.parse(sDateEnd);
+
+        long days = DateUtil.between(dateStart, dateEnd, DateUnit.DAY);
+        years = days/365f;
+        return years;
     }
 }
--- trend-trading-backtest-service/src/main/java/com/ming/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/com/ming/web/BackTestController.java
@@ -3,6 +3,8 @@ package com.ming.web;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import com.ming.pojo.IndexData;
+import com.ming.pojo.Profit;
+import com.ming.pojo.Trade;
 import com.ming.service.BackTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -31,14 +33,44 @@ public class BackTestController {
 
         allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
 
+        int ma = 20;
+        float sellRate = 0.95f;
+        float buyRate = 1.05f;
+        float serviceCharge = 0f;
+        Map<String,?> simulateResult= backTestService.simulate(ma,sellRate, buyRate,serviceCharge, allIndexDatas);
+        List<Profit> profits = (List<Profit>) simulateResult.get("profits");
+        List<Trade> trades = (List<Trade>) simulateResult.get("trades");
+
+        float years = backTestService.getYear(allIndexDatas);
+        float indexIncomeTotal = (allIndexDatas.get(allIndexDatas.size()-1).getClosePoint() - allIndexDatas.get(0).getClosePoint()) / allIndexDatas.get(0).getClosePoint();
+        float indexIncomeAnnual = (float) Math.pow(1+indexIncomeTotal, 1/years) - 1;
+        float trendIncomeTotal = (profits.get(profits.size()-1).getValue() - profits.get(0).getValue()) / profits.get(0).getValue();
+        float trendIncomeAnnual = (float) Math.pow(1+trendIncomeTotal, 1/years) - 1;
+
+        int winCount = (Integer) simulateResult.get("winCount");
+        int lossCount = (Integer) simulateResult.get("lossCount");
+        float avgWinRate = (Float) simulateResult.get("avgWinRate");
+        float avgLossRate = (Float) simulateResult.get("avgLossRate");
+
         Map<String,Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
         result.put("indexStartDate", indexStartDate);
         result.put("indexEndDate", indexEndDate);
+        result.put("profits", profits);
+        result.put("trades", trades);
+        result.put("years", years);
+        result.put("indexIncomeTotal", indexIncomeTotal);
+        result.put("indexIncomeAnnual", indexIncomeAnnual);
+        result.put("trendIncomeTotal", trendIncomeTotal);
+        result.put("trendIncomeAnnual", trendIncomeAnnual);
+
+        result.put("winCount", winCount);
+        result.put("lossCount", lossCount);
+        result.put("avgWinRate", avgWinRate);
+        result.put("avgLossRate", avgLossRate);
 
         return result;
     }
-
 
     private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
         if(StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate) )
@@ -59,5 +91,4 @@ public class BackTestController {
         }
         return result;
     }
-
 }
